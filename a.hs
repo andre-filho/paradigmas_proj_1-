50dc8f35dc78366f1ee377d1b7c229980faefb3f
Andre Filho <andre.filho001@outlook.com>
2019-04-06 10:21:52 -0300

    Adds more functions to the add

--- a.hs
+++ a.hs
@@ -2,7 +2,5 @@ import Data.List
 
 main = do
     a <- readFile "LICENSE"
-    let b = lines a
-    print b
-
-limpa (h:t) b = if h /= "" then h : b else limpa t
+    -- let b = lines a
+    print a
--- legit.hs
+++ legit.hs
@@ -4,6 +4,16 @@ import System.Directory
 import qualified Data.ByteString.Lazy as BS
 import qualified Crypto.Hash.SHA1 as SHA1
 
+data UnixTime
+
+split :: String -> Char -> [String]
+split [] delim = [""]
+split (c:cs) delim
+    | c == delim = "" : rest
+    | otherwise = (c : head rest) : tail rest
+    where
+        rest = split cs delim
+
 -- verifica se o arquivo ja fora adicionado no legit antes
 -- chamar o segundo argumento com a funcao `lines x`
 verifyPresenceInLegit :: String -> [String] -> Bool     -- funfando nice
@@ -11,26 +21,23 @@ verifyPresenceInLegit a [] = False
 verifyPresenceInLegit a (h:t) = if (h == a) then True else verifyPresenceInLegit a t
 
 -- verifica se existe diferença entre o arquivo atual e na pasta legit
-verifiyDiferenceInLegit :: String -> String -> Bool     -- funfando nice
-verifiyDiferenceInLegit a b = a == b
+verifyDiferenceInLegit :: String -> String -> Bool     -- funfando nice
+verifyDiferenceInLegit a b = a == b
 
 genAddHash :: String -> String -> String    -- funfando
 genAddHash a b = a ++ ":" ++ b
 
-writeAddToFile s1 s2 = do   -- funfando
-    a <- openFile "asdf.txt" AppendMode
+writeAddHashToFile :: IO () -> String -> String -> IO ()   -- funfando
+writeAddHashToFile filename s1 s2 = do
+    a <- openFile filename AppendMode
     let c = genAddHash s1 s2
     hPutStrLn a c
     hClose a 
-    
--- Arquiva o commit como o arquivo na pasta .gitLegit
--- genCommit :: 
 
--- -- transforma o conteúdo de um arquivo em uma gigantesca string
--- fileToString filename
+-- verifica se existe a pasta .gitlegit e a cria se não existir 
+createFolder ::  IO ()  -- funfando
+createFolder = System.Directory.createDirectoryIfMissing True "./.legit/archive"
 
--- -- verifica se existe a pasta .gitlegit e a cria se não existir
--- createFolder = 
-
--- gitAdd = do
---     var <- getLine
+createFile c1 c2 = do
+    -- let timestamp = (round . (* 1000)) <$> getPOSIXTime 
+    writeAddHashToFile "./.legit/archive/add.txt" c1 c2
